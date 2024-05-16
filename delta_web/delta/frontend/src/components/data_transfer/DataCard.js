c8e3e2d19c8fa8daf8034cc7c314c107cbc0c6a2
lxaw <whalenlex@gmail.com>
2024-05-16 13:05:14 -0400

    better cart

--- delta_web/delta/frontend/src/components/data_transfer/DataCard.js
+++ delta_web/delta/frontend/src/components/data_transfer/DataCard.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import styles from "./tags.module.css";
 
-const DataCard = ({ data, isDownload, link, linkText }) => {
+const DataCard = ({ data}) => {
+  const [isHovered, setIsHovered] = useState(false);
   const cardStyle = {
-    backgroundColor: isDownload ? '#cce6ff' : 'white',
+    backgroundColor: isHovered ?'#f5f5f5':'white',
     transition: 'background-color 0.3s',
     height: '20rem',
-    width: '30rem',
+    width: '26rem',
+    margin:'0.5rem'
   };
 
   const descriptionStyle = {
@@ -16,53 +18,52 @@ const DataCard = ({ data, isDownload, link, linkText }) => {
     paddingRight: '1rem',
   };
 
-  const tagStyle = {
-    display: 'inline-block',
-    backgroundColor: '#f0f0f0',
-    color: '#333',
-    borderRadius: '4px',
-    marginRight: '4px',
-    marginBottom: '4px',
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
   };
 
   return (
-    <div className="card" style={cardStyle} data-testid="data_card-1">
-      <div className="card-body">
-        <div className="d-flex justify-content-between align-items-start mb-3">
-          <div>
-            <h5 className="card-title">{data.name}</h5>
-            <div>Rating: {data.avg_rating}</div>
-            <div>Download count: {data.download_count}</div>
-          </div>
-          <div className="text-end">
-            <Link to={`/profile/${data.author_username}`}>
-              {data.author_username}
-            </Link>
-            <div className="text-muted">{data.formatted_date}</div>
-          </div>
-        </div>
-        <p className="card-text" style={descriptionStyle}>
-          {data.description}
-        </p>
-        <div className="row mt-auto">
-          <div className="col-8">
-            <strong>Tags:</strong>
-            <div className="mt-1">
-              {data.tags.map((tag, index) => (
-              <div className={styles.tag_item} key={index}>
-                <span className={styles.text}>{tag.text}</span>
-              </div>
-              ))}
+    <Link to={`/csvs/${data.id}`} style = {{textDecoration:'none',color:'inherit'}}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="card" style={cardStyle} data-testid="data_card-1">
+        <div className="card-body">
+          <div className="d-flex justify-content-between align-items-start mb-3">
+            <div>
+              <h5 className="card-title">{data.name}</h5>
+              <div>Rating: {data.avg_rating}</div>
+              <div>Download count: {data.download_count}</div>
+            </div>
+            <div className="text-end">
+              <Link to={`/profile/${data.author_username}`}>
+                {data.author_username}
+              </Link>
+              <div className="text-muted">{data.formatted_date}</div>
             </div>
           </div>
-          <div className="col-4 d-flex justify-content-end align-items-end">
-            <Link to={link} className="btn btn-sm btn-primary">
-              {linkText}
-            </Link>
+          <p className="card-text" style={descriptionStyle}>
+            {data.description}
+          </p>
+          <div className="row mt-auto">
+            <div className="col-8">
+              <strong>Tags:</strong>
+              <div className="mt-1">
+                {data.tags.map((tag, index) => (
+                <div className={styles.tag_item} key={index}>
+                  <span className={styles.text}>{tag.text}</span>
+                </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
