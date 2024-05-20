4a4c0a419374124e70baada8bf98114cffcdcab0
lxaw <whalenlex@gmail.com>
2024-05-20 13:18:36 -0400

    updated stars on the files, began download count

--- delta_web/delta/frontend/src/components/data_transfer/DataCard.js
+++ delta_web/delta/frontend/src/components/data_transfer/DataCard.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import styles from "./tags.module.css";
+import { FaStar,FaArrowDown } from 'react-icons/fa';
 
 const DataCard = ({ data}) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -26,6 +27,35 @@ const DataCard = ({ data}) => {
     setIsHovered(false);
   };
 
+  const renderStars = (rating) =>{
+    var stars = []
+    if(rating == 0){
+      stars = Array.from({ length: 5 }, (_, i) => (
+      <FaStar
+        key={i}
+        color={i <= rating ? "gold" : "gray"}
+        className="mr-1"
+      />
+      ));
+    }
+    else{
+      if(rating == 0){
+        // not rated yet
+        stars = [<FaStar color={"gray"} className="mr-1"/>]
+      }
+      for (let i=0;i<5;i++){
+        stars.push(
+          <FaStar
+            key={i}
+            color={i <= rating ? "gold":"gray"}
+            className="mr-1"
+          />
+        )
+      }
+    }
+    return stars
+  }
+
   return (
     <Link to={`/csvs/${data.id}`} style = {{textDecoration:'none',color:'inherit'}}
       onMouseEnter={handleMouseEnter}
@@ -36,8 +66,13 @@ const DataCard = ({ data}) => {
           <div className="d-flex justify-content-between align-items-start mb-3">
             <div>
               <h5 className="card-title">{data.name}</h5>
-              <div>Rating: {data.avg_rating}</div>
-              <div>Download count: {data.download_count}</div>
+              <div className="d-flex">{renderStars(data.avg_rating-1)}</div>
+            </div>
+            <div className="d-flex align-items-center position-relative">
+              <FaArrowDown className="text-muted" style={{ fontSize: '1.5rem' }} />
+              <span className="position-absolute top-50 start-50 translate-middle badge bg-secondary">
+                {data.download_count}
+              </span>
             </div>
             <div className="text-end">
               <Link to={`/profile/${data.author_username}`}>
