ef291c630d3713b8908d95c22b0d2d7bfc6fb532
lxaw <whalenlex@gmail.com>
2024-06-10 12:02:23 -0400

    can view files in folder now

--- delta_web/delta/frontend/src/components/profile/FolderDetail.js
+++ delta_web/delta/frontend/src/components/profile/FolderDetail.js
@@ -1,26 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getFolderById } from "../../actions/file";
+import DataCard from "../data_transfer/DataCard";
 
 const FolderDetail = (props) => {
     const { id } = useParams();
+    const [folder, setFolder] = useState(null);
+    const [datasets, setDatasets] = useState([]);
+    const navigate = useNavigate();
 
-    useEffect(()=>{
-        props.getFolderById(id).then((res)=>{
-            console.log(res)
-        })
-    })
+    useEffect(() => {
+        props.getFolderById(id).then((res) => {
+            setFolder(res.data);
+            setDatasets(res.data.datasets);
+        });
+    }, [id, props]);
+
+    const renderItems = () => {
+        return datasets.map((item) => {
+            const handleClick = () => {
+                navigate(`/csvs/${item.id}`);
+            };
+
+            return (
+                <div className="col-4" key={item.id}>
+                    <span onClick={handleClick}>
+                        <DataCard data={item} />
+                    </span>
+                </div>
+            );
+        });
+    };
 
     return (
         <div>
-            Folder ID: {id}
+            {folder && (
+                <div>
+                    <h2>{folder.name}</h2>
+                    <p>{folder.description}</p>
+                </div>
+            )}
+            <div className="row">
+                {datasets.length > 0 ? renderItems() : <p>No datasets available</p>}
+            </div>
         </div>
     );
-}
+};
 
 const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, {getFolderById})(FolderDetail);
+export default connect(mapStateToProps, { getFolderById })(FolderDetail);
